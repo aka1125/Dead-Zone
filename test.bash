5aeda201f56acf2efde1298712fa2eb79dd3b84e
aka1125 <akayuusaku91@gmail.com>
2025-11-13 20:02:07 +0900

    add test and dead_zone

---
+++ test.bash
@@ -0,0 +1,43 @@
+#!/bin/bash -xv
+# SPDX-FileCopyrightText: 2025 Yusaku Aka
+# SPDX-License-Identifier: BSD-3-Clause
+
+ng () {
+	echo ${1}行目が違うよ
+	res=1
+}
+
+res=0
+
+
+### NORMAL INSERT ###
+
+input_data=$(printf "1.00\n1.02\n1.11\n1.23\n")
+output_data=$(printf "1.0\n1.0\n1.11\n1.23\n")
+
+output=$(echo "$input_data" | python3 ./dead_zone.py)
+test "$output" = "$output_data" || ng "$LINENO"
+
+### STRANGE INSERT ###
+
+#CASE1
+input_str="あ\nい\nう\nえ\nお\n"
+output=$(echo "$input_str" | python3 ./dead_zone.py)
+
+test "$?" = 1 || ng "$LINENO"
+test "$output" = "" || ng "$LINENO"
+
+#CASE2
+input_emp="\n"
+output=$(echo "$input_emp" | python3 ./dead_zone.py)
+
+test "$?" = 1 || ng "$LINENO"
+test "$output" = "" || ng "$LINENO"
+
+output=$(echo "$input_str" | python3 ./dead_zone.py)
+
+test "$?" = 1 || ng "$LINENO"
+test "$output" = "" || ng "$LINENO"
+
+test "$res" = 0 && echo OK
+exit $res
